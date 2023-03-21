98e624911895d112568437efa09248f192f2accf
Ubuntu <PFE@PFE.2st1zkf0kujuxltzm2tq10olbg.ux.internal.cloudapp.net>
2023-03-21 18:55:08 +0000

    delete mysql dir

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -86,10 +86,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME', 'django-app-db'),
-        'USER': os.environ.get('DB_USER', 'djawed'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'djawed'),
-        'HOST': os.environ.get('DB_HOST', 'mysql-service'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': int(os.environ.get('DB_PORT', 3306)),
 
     }
